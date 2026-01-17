e0c88c5818a179fd851e8ca8fdfe8adbe89da787
吴天一 <faithleysath@gmail.com>
2026-01-17 19:33:41 +0800

    feat: update NapCatQQ submodule and add ts-json-schema-generator dependency

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+// _temp_extract_Payload.ts
+
+import { FetchEmojiLike } from './FetchEmojiLike';
+import { OneBotAction } from 'napcat-onebot/action/OneBotAction';
+
+// 提取第一个泛型 P (Payload)
+type ExtractPayload<T> = T extends OneBotAction<infer P, any> ? P : never;
+
+type ActionInstance = InstanceType<typeof FetchEmojiLike>;
+
+// 导出 Payload
+export type __Target_Payload_Type__ = ExtractPayload<ActionInstance>;
