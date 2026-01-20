dead4c5818b2ecfca5b2bd0c09f29c4ee6d5fc0e
吴天一 <faithleysath@gmail.com>
2026-01-20 14:41:58 +0800

    Refactor code structure for improved readability and maintainability

--- scripts/api-codegen.py
+++ scripts/api-codegen.py
@@ -30,6 +30,14 @@ content = re.sub(pattern, "\n", content)
 with open(api_schema_path, "r") as f:
     api_schema = json.load(f)
 
+with open("apifox.openapi.json", "r") as f:
+    apifox_schema = json.load(f)
+
+docstring_map = {
+    path.lstrip("/"): f'        """\n        {data.get("post", {}).get("summary", "")}\n\n        标签: {(data.get("post", {}).get("tags") or [""])[0]}\n        """'
+    for path, data in apifox_schema.get("paths", {}).items()
+}
+
 typemap = {
     "string": "str",
     "number": "float",
@@ -37,11 +45,29 @@ typemap = {
     "null": "None",
 }
 
+client_api_code = """# Auto-generated file. Do not modify directly.
+# 自动生成的文件。请勿直接修改。
+
+from collections.abc import Mapping
+from typing import Any, Unpack, Protocol
+from .types.schemas import (
+
+"""
+
+api_func_code = ""
+
 for endpoint in api_schema["paths"].values():
     method = endpoint.get("post", {})
     operation_id = method.get("operationId", "")
     RequestClassName = snake_to_classname(operation_id) + "PostRequest"
     ResponseClassName = snake_to_classname(operation_id) + "PostResponse"
+    client_api_code += f"   {RequestClassName},\n   {ResponseClassName},\n"
+    api_func_code += f"""
+    async def {operation_id.replace('.', 'dot_')}(self, **kwargs: Unpack[{RequestClassName}]) -> {ResponseClassName}:
+{docstring_map.get(operation_id, '        \"\"\"\n        未提供描述\n        \"\"\"')}
+        return await self._client.call_action("{operation_id}", kwargs)
+    """
+
     requestSchema = method["requestBody"]["content"]["application/json"]["schema"]
     if not requestSchema:
         content += f"\n\nclass {RequestClassName}(TypedDict):\n    pass\n"
@@ -53,5 +79,25 @@ for endpoint in api_schema["paths"].values():
         content += f"\n\ntype {ResponseClassName} = {typemap[responseSchema['type']]}\n"
         continue
 
+client_api_code += """)
+# 定义一个 Protocol，避免循环导入 Client 类，同时保证类型提示
+class CallActionProtocol(Protocol):
+    async def call_action(self, action: str, params: Mapping[str, Any] | None = None) -> Any: ...
+
+class NapCatAPI:
+    \"\"\"
+    NapCat API 命名空间。
+    所有自动生成的方法都挂载于此，通过 client.api.xxx 调用。
+    \"\"\"
+
+    def __init__(self, client: CallActionProtocol):
+        self._client = client
+
+{api_func_code}
+"""
+
 with open(api_schema_code_path, "w") as f:
     f.write(content)
+
+with open("src/napcat/client_api.py", "w") as f:
+    f.write(client_api_code)
