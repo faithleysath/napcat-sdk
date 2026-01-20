8867106e6144848e0e6d0b2a95f875abbb080406
吴天一 <faithleysath@gmail.com>
2026-01-20 13:55:11 +0800

    feat: add new API endpoints for OCR image processing and quick operations

--- scripts/generate_schema.py
+++ scripts/generate_schema.py
@@ -4,6 +4,10 @@ import tomllib
 import json
 
 def snake_to_classname(s: str) -> str:
+    if s.startswith("_"):
+        s = "field" + s
+    if s.startswith("."):
+        s = "field_" + s[1:]
     parts = s.split('_')
     return ''.join(word[0].upper() + word[1:] for word in parts if word)
 
@@ -25,22 +29,28 @@ content = re.sub(pattern, "\n", content)
 with open(api_schema_path, "r") as f:
     api_schema = json.load(f)
 
+typemap = {
+    "string": "str",
+    "number": "float",
+    "boolean": "bool",
+    "null": "None",
+}
+
 for endpoint in api_schema["paths"].values():
     method = endpoint.get("post", {})
     operation_id = method.get("operationId", "")
     RequestClassName = snake_to_classname(operation_id) + "PostRequest"
     ResponseClassName = snake_to_classname(operation_id) + "PostResponse"
-    if not method["requestBody"]["content"]["application/json"]["schema"]:
+    requestSchema = method["requestBody"]["content"]["application/json"]["schema"]
+    if not requestSchema:
         content += f"\n\nclass {RequestClassName}(TypedDict):\n    pass\n"
-        continue
-    if not method["responses"]["200"]["content"]["application/json"]["schema"]:
+    responseSchema = method["responses"]["200"]["content"]["application/json"]["schema"]
+    if not responseSchema:
         content += f"\n\ntype {ResponseClassName} = Any\n"
         continue
-    if method["responses"]["200"]["content"]["application/json"]["schema"].get("type", "") == "null":
-        content += f"\n\ntype {ResponseClassName} = None\n"
+    if responseSchema.get("type", "") in typemap:
+        content += f"\n\ntype {ResponseClassName} = {typemap[responseSchema['type']]}\n"
         continue
-
-    
 
 with open(api_schema_code_path, "w") as f:
     f.write(content)
