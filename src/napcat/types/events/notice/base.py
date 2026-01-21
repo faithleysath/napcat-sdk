5c9f89c0533b906af1c6eda7d9526457e664f2ab
吴天一 <faithleysath@gmail.com>
2026-01-21 12:20:45 +0800

    refactor: remove unnecessary blank line in NoticeEvent class

--- src/napcat/types/events/notice/base.py
+++ src/napcat/types/events/notice/base.py
@@ -24,7 +24,6 @@ class NoticeEvent(NapCatEvent):
     _notify_registry: ClassVar[dict[str, type[NoticeEvent]]] = {}
 
     def __init_subclass__(cls, **kwargs: Any):
-
         # 1. 获取 notice_type
         n_type = getattr(cls, "notice_type", None)
         if not n_type or not isinstance(n_type, str):
