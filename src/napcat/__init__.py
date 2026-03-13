a145e057ab5c1eb1ac076a21db3747201aec83b6
吴天一 <faithleysath@gmail.com>
2026-03-13 20:28:54 +0800

    chore: Bump version to 0.6.6 in pyproject.toml and __init__.py

--- src/napcat/__init__.py
+++ src/napcat/__init__.py
@@ -12,7 +12,7 @@ NapCat SDK 是一个用于与 NapCatQQ (OneBot 11) 进行交互的 Python SDK。
 
 # src/napcat/__init__.py
 
-__version__ = "0.6.5"
+__version__ = "0.6.6"
 
 from . import types
 from .client import NapCatClient
